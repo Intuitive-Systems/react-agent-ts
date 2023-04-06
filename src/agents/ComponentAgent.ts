a19eba0de9b847428ea1a1db5f705747a8d2af34
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2023-04-06 10:09:43 -0700

    cleaning up

--- src/agents/ComponentAgent.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentEngine } from "../engines/ComponentEngine";
-import { BaseAgent } from "./base";
-
-export class ComponentAgent extends BaseAgent<ComponentEngine>{
-    constructor() {
-        const engine = new ComponentEngine();
-        super(engine);
-    }
-}
--- src/server/schema.ts
+++ src/server/schema.ts
@@ -1,8 +1,6 @@
 import { GraphQLSchema, GraphQLString, GraphQLObjectType, GraphQLInt } from 'graphql';
 import {ReactAgent} from '../agents/ReactAgent';
-import {ComponentAgent} from '../agents/ComponentAgent';
 const reactAgent = new ReactAgent();
-const componentAgent = new ComponentAgent();
 
 export const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -24,24 +22,6 @@ export const schema = new GraphQLSchema({
                 type: GraphQLString,
                 resolve: async (parent: any, args: any) => {
                     const response = await reactAgent.reset();
-                    return "ok";
-                }
-            },
-            componentMessage: {
-                type: GraphQLString,
-                args: {
-                    userInput: { type: GraphQLString },
-                },
-                resolve: async (parent: any, args: any) => {
-                    const userInput = args.userInput;
-                    const response = await componentAgent.addMessage(userInput);
-                    return response;
-                }
-            },
-            resetComponent: {
-                type: GraphQLString,
-                resolve: async (parent: any, args: any) => {
-                    const response = await componentAgent.reset();
                     return "ok";
                 }
             }
